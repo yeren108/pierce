f439c02f2511bdb3623101bc8590b8431751c264
liubiao <yeren108@icloud.com>
2019-12-11 14:36:15 +0800

    initialize;

---
+++ src/main/java/com/yeren/pierce/PierceApplication.java
@@ -0,0 +1,13 @@
+package com.yeren.pierce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PierceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PierceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeren/pierce/PierceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeren.pierce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PierceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
